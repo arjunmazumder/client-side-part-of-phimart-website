bbc7081e55eec10f680c12942413dba9489dd522
Arjun Mazumder <arjumazumder2@gmail.com>
2025-12-29 10:35:23 +0600

    module31.5 react router navigation, tailwindcss and dasiy ui setup

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link, NavLink } from "react-router";
 const Navbar = () => {
     return (
         <div>
+            <button className="btn btn-primary">Primary</button>
             <NavLink className={({ isActive }) => isActive ? "text-red-500" : ""}  to='/'>Home</NavLink >
             <NavLink className={({isActive}) => isActive ? "text-red-500" : ""}  to='/products'>Product</NavLink >
             <NavLink className={({isActive}) => isActive ? "text-red-500" : ""} to='/about'>About</NavLink >
