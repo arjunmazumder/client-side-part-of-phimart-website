4719771afa1f8424b8edfe237b311258bf3ddf28
Arjun Mazumder <arjumazumder2@gmail.com>
2025-12-31 13:15:14 +0600

    module31.11  Creating features component single page application can work without refresh.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 
+import { Features } from 'tailwindcss';
 import HeroCarousel from '../../components/carousel/HeroCarousel';
+import Feature from '../../components/Feature';
 
 const Home = () => {
     return (
         <div>
             <HeroCarousel></HeroCarousel>
+            <Feature></Feature>
             
         </div>
     );
